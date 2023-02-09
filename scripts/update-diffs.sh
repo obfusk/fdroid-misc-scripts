65835b09b72b4a4b7e46d1def03e408751ca9086
FC Stegerman <flx@obfusk.net>
2023-02-09 12:56:27 +0100

    update-diffs.sh: skip existing files

--- scripts/update-diffs.sh
+++ scripts/update-diffs.sh
@@ -11,8 +11,18 @@ while (( i < l - 1 )); do
   j=$(( i + 1 ))
   x="${files[i]}" y="${files[j]}"
   z="${y%-apps}"
-  diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$z"-rems
-  diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$z"-adds
+  if [ -e "$z"-rems ]; then
+    echo "skipping $z-rems"
+  else
+    echo "creating $z-rems ..."
+    diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$z"-rems
+  fi
+  if [ -e "$z"-adds ]; then
+    echo "skipping $z-adds"
+  else
+    echo "creating $z-adds ..."
+    diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$z"-adds
+  fi
   (( ++i ))
 done
 popd # stats
@@ -23,8 +33,18 @@ i=0 l=${#files[@]}
 while (( i < l - 1 )); do
   j=$(( i + 1 ))
   x="${files[i]}" y="${files[j]}"
-  diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$y"-rems
-  diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$y"-adds
+  if [ -e "$y"-rems ]; then
+    echo "skipping $y-rems"
+  else
+    echo "creating $y-rems ..."
+    diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$y"-rems
+  fi
+  if [ -e "$y"-adds ]; then
+    echo "skipping $y-adds"
+  else
+    echo "creating $y-adds ..."
+    diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$y"-adds
+  fi
   (( ++i ))
 done
 files=( *-sigs )
@@ -32,8 +52,18 @@ i=0 l=${#files[@]}
 while (( i < l - 1 )); do
   j=$(( i + 1 ))
   x="${files[i]}" y="${files[j]}"
-  diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$y"-rems
-  diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$y"-adds
+  if [ -e "$y"-rems ]; then
+    echo "skipping $y-rems"
+  else
+    echo "creating $y-rems ..."
+    diff -Naur "$x" "$y" | grep ^- | cut -c2- | tail -n +2 > "$y"-rems
+  fi
+  if [ -e "$y"-adds ]; then
+    echo "skipping $y-adds"
+  else
+    echo "creating $y-adds ..."
+    diff -Naur "$x" "$y" | grep ^+ | cut -c2- | tail -n +2 > "$y"-adds
+  fi
   (( ++i ))
 done
 popd # reproducible
