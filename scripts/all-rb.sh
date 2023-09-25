118f46eb510e2fcc315c069095d58ac481e2bf6f
FC Stegerman <flx@obfusk.net>
2023-09-25 21:51:40 +0200

    add & run all-rb.sh

---
+++ scripts/all-rb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+firstofthismonth="$( date +%Y-%m-01 )"
+date="${1:-$firstofthismonth}"
+{
+  echo "# All F-Droid apps published with Reproducible Builds ($date)"
+  echo
+  cat reproducible/"$date"-{bins,sigs} | sort \
+    | grep -Ev 'disabled|archived|missing' \
+    | sed -r 's#^(\S+)#* [`\1`](https://f-droid.org/packages/\1)#'
+} > reproducible/"$date"-all.md
